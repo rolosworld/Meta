0fca5e8b173e5341526fd11182593cbf6fe53b98
Rolando Gonzalez-Chevere <rolosworld@gmail.com>
2018-11-06 20:07:14 -0500

    - Update docs JS code

--- docs/meta.debug.js
+++ docs/meta.debug.js
@@ -1364,7 +1364,7 @@ Meta.string=Meta(Meta.core).extend({
   */
   isInt:function()
   {
-    return this.get().match(/^\d$/g) !== null;
+    return this.get().match(/^\d+$/g) !== null;
   },
 
   /**
